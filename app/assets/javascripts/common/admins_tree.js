14ee267ea453d68326bb0a539a69e50f1cce81a7
Dany Sam Salmon <danisam17@gmail.com>
2020-10-23 20:44:27 +0530

    Improve naming for AccesTree and add some ref docs

--- app/assets/javascripts/common/admins_tree.js
+++ app/assets/javascripts/common/admins_tree.js
@@ -1,5 +1,6 @@
 const AccessTree = function () {
   const self = this;
+  const ACCESS_ITEM_SELECTOR = ".access-item";
 
   const hierarchy = {
     organization: {
@@ -24,11 +25,11 @@ const AccessTree = function () {
     return hierarchy[accessType] && hierarchy[accessType].parent;
   }
 
-  function addAccessItemMetaData(result, accessItem, _key) {
+  function addAccessItemMetaData(accessGroup, accessItem, _key) {
     const accessType = accessItem.dataset.accessType;
     const parentKey = getParentKey(accessType);
     const parentId = accessItem.dataset[parentKey];
-    return (result[accessItem.dataset.id] = {
+    return (accessGroup[accessItem.dataset.id] = {
       element: accessItem,
       [parentKey]: parentId,
       name: accessItem.dataset.name,
@@ -65,19 +66,19 @@ const AccessTree = function () {
       const childKey = getChildKey(accessType);
       const parentIdKey = getParentKey(childKey);
       if (!childKey) return;
-      return Object.values(tree[childKey]).filter(
-        (item) => item[parentIdKey] === itemId
-      );
+      return Object.values(tree[childKey])
+        .filter(item => item[parentIdKey] === itemId);
     };
   }
 
   function buildAccessTree() {
-    const accessItems = Array.from(document.querySelectorAll(".access-item"));
+    const accessItems = Array.from(document.querySelectorAll(ACCESS_ITEM_SELECTOR));
     const data = _.groupBy(accessItems, (item) => item.dataset.accessType);
     return _.transform(
       data,
-      (result, accessItem, key) =>
-        (result[key] = _.transform(accessItem, addAccessItemMetaData, {}))
+      (accessTree, accessGroup, accessType) => {
+        accessTree[accessType] = _.transform(accessGroup, addAccessItemMetaData, {})
+      }
     );
   }
 
@@ -89,3 +90,31 @@ const AccessTree = function () {
     facilityGroup: this.accessTree.facilityGroup,
   };
 };
+
+/**
+ * Notes for integrating this with the admin.js
+ * --------------------------------------------
+ *
+ * Running new AccessTree() would return a tree with accessItems grouped by
+ * accessGroup (organization, facilityGroup, facility).
+ *
+ * The object would look something like this
+ * ```Object { organization: {…}, facility: {…}, facilityGroup: {…} }```
+ *
+ * Each item in the accessGroup is a DOMElement element wrapped with some helper
+ * functions, namely .parent(), .children(), .siblings() to help walk up and down
+ * the tree.
+ *
+ * Adding a new hierarchy
+ * ----------------------
+ *
+ * Adding the new hierarchy should be pretty straightforward. You'll need to
+ * add it to the hierarchy object in AccessTree, and adding the following data
+ * attributes to the new access item partial: data-<parent-access-type>,
+ * data-access-type, data-id, data-name
+ *
+ * data-<parent-access-type> holds the ID of the parent
+ * 
+ * That should be it.
+ * Cheers!
+ */
