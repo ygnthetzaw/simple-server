85086c3bdf908117e76619881edcd208c2b62bff
kpethtel <kris.pethtel@gmail.com>
2021-02-24 12:01:47 -0500

    2116: update password setting UI to use validation checklist

--- app/assets/javascripts/common/password_validation.js
+++ app/assets/javascripts/common/password_validation.js
@@ -1,9 +1,10 @@
 PasswordValidation = function() {
   const DebounceTimeout = 500;
+  const DefaultResult = {length: false, lower: false, upper: false, number: false}
 
   this.initialize = function() {
     this.timer = null;
-    this.response = null;
+    this.result = DefaultResult;
     this.passwordInput = $("#password");
     this.passwordInput.on("input", this.handlePasswordInput);
   }
@@ -39,15 +40,43 @@ PasswordValidation = function() {
         "X-CSRF-Token": token
       },
       data: {"password": password}
-    }).done(function(data, status){
+    }).done((data, status) => {
       console.log(status)
       console.log(data)
       this.timer = null;
       if (status === "success") {
-        this.response = data["errors"];
+        this.updateResults(data["errors"]);
       } else {
-        this.response = null;
+        this.result = DefaultResult;
       }
+      this.updateChecklist();
     });
+
+    this.updateResults = function(response) {
+      this.result["length"] = response.includes("must be between 10 and 128 characters");
+      this.result["lower"] = response.includes("must contain at least one lower case letter");
+      this.result["upper"] = response.includes("must contain at least one upper case letter");
+      this.result["number"] = response.includes("must contain at least one number");
+    }
+
+    this.updateChecklist = function() {
+      this.result["length"] ? this.uncheckItem("length") : this.checkItem("length")
+      this.result["lower"]? this.uncheckItem("lower") : this.checkItem("lower")
+      this.result["upper"] ? this.uncheckItem("upper") : this.checkItem("upper")
+      this.result["number"] ? this.uncheckItem("number") : this.checkItem("number")
+    }
+
+    this.checkItem = function(id) {
+      const item = $(`#${id}`);
+      if (item.hasClass("complete")) return
+      item.addClass("complete");
+    }
+
+    this.uncheckItem = function(id) {
+      const item = $(`#${id}`);
+      if (!item.hasClass("complete")) return
+      item.removeClass("complete");
+    }
+
   }
 }
