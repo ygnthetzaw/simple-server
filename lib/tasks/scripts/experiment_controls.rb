0ab85dc2223a0296452c7d5814231f90ccdbf98d
kpethtel <kris.pethtel@gmail.com>
2021-03-19 12:32:47 -0400

    fix query

--- lib/tasks/scripts/experiment_controls.rb
+++ lib/tasks/scripts/experiment_controls.rb
@@ -1,7 +1,7 @@
 module ExperimentControls
   def self.start_active_patient_experiment(percentage_of_patients)
-    eligible = Patient.with_hypertension.contactable.where("age >= ?", 18)
-    experiment_patient_count = (0.01 * percentage_of_patients * eligible.count).round
+    eligible = Patient.from(Patient.with_hypertension, :patients).contactable.where("age >= ?", 18)
+    experiment_patient_count = (0.01 * percentage_of_patients * eligible.length).round
     experiment_patients = eligible.take(experiment_patient_count)
     experiment_patients.each do |patient|
       Flipper.enable(:experiment, patient)
